5c1dcd1f3c92f7e920335e31b19e8ed82f08abdd
Jonczyk <krzysztof.jonczyk@gmail.com>
2019-02-04 19:59:38 +0100

    init even more

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <form @submit="addTodo">
+            <input type="text" name="title" placeholder="Add Todo..." v-model="title">
+            <input type="submit" value="Submit" class="btn">
+        </form>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "AddTodo",
+    methods: {
+        addTodo(e) {
+            e.preventDefault();
+            const newTodo = {
+                title: this.title,
+                completed: false
+            }
+            //Send to parent
+            this.$emit('add-todo', newTodo);
+            this.title = '';
+        }
+    },
+    data() {
+        return {
+            title: ''
+        }
+    }
+}
+</script>
+
+<style scoped>
+    form {
+        display: flex;
+    }
+
+    input[type="text"] {
+        flex: 10px;
+        padding: 5px;
+    }
+
+    input[type="submit"] {
+        flex: 2px;
+    }
+</style>
+
---
+++ src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>
+      This is todoList v0.1.0. It's made with love using Vue
+    </p>  
+  </div>
+</template>
